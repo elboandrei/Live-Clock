078835f04a647695ef840c0d95d4730a72669346
Andrei Elbo <andreijce@gmail.com>
2022-08-27 16:47:46 -0400

    liveclock v1

---
+++ Javascript/index.js
@@ -0,0 +1,21 @@
+function Clock() {
+    var liveClock = new Date();
+
+    var hours = liveClock.getHours();
+    var minutes = liveClock.getMinutes();
+    var seconds = liveClock.getSeconds();
+
+
+    //
+    var amPm = (hours < 12) ? "AM" : "PM";
+    hours = (hours > 12 ) ? hours - 12 : hours;
+
+    hours = ("0" + hours).slice(-2);
+    minutes = ("0" + minutes).slice(-2);
+    seconds = ("0" + seconds).slice(-2);
+
+    document.getElementById('clock').innerHTML = 
+        hours + " : " + minutes + " : " + seconds + " " + amPm;
+
+    var t = setTimeout(Clock, 500);
+}
